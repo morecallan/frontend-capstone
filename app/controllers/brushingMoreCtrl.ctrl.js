d216d7bab8f136e7e00e1c326e5ed241319f2540
Callan Morrison <callan.l.morrison@gmail.com>
2016-06-18 01:55:25 -0500

    updated calendar styling

--- app/controllers/brushingMoreCtrl.ctrl.js
+++ app/controllers/brushingMoreCtrl.ctrl.js
@@ -6,37 +6,37 @@ app.controller('brushingMoreCtrl', function ($scope, $location, $rootScope, $rou
     /********************************************
     **               SELECTED USER             **
     ********************************************/
-    $scope.usersBrushingData = "";
-
-    $('#calendar').fullCalendar("changeView", "basicWeek");
-
-    Date.prototype.addHours = function(h) {    
-       this.setTime(this.getTime() + (h*60*60*1000)); 
-       return this;   
-    };
+    $('#calendar').fullCalendar("defaultView", "basicWeek");
 
 
     $scope.events = [];
 
-    $scope.getUserBrushingData = () => {
+    $scope.populateBrushingData = () => {
         brushingDataFactory.returnAllBrushingDataForChild().then((returnBrushingData)=>{
-            $scope.usersBrushingData = returnBrushingData;
-            $scope.eventPlaceholder = returnBrushingData;
+            $scope.renderDataForCalendar(returnBrushingData);
+        });         
+    };
+
+    $scope.renderDataForCalendar = (returnBrushingData) => {
+        $scope.eventPlaceholder = returnBrushingData;
             $('#calendar').fullCalendar("changeView", "basicWeek");
             $scope.eventPlaceholder.forEach((event)=>{
                 event.title = "I Brushed My Teeth!";
-                event.start = event.timestamp;
-                let datePlusHrs = new Date(event.timestamp).addHours();
-                event.end = datePlusHrs;
+                event.start = new Date(event.timestamp);
+                let startTime = new Date(event.timestamp);
+                let newDateObj = new Date(startTime.getTime() + (480*60000));
+                event.end = newDateObj;
                 event.allDay = false;
                 event.className = "stickerEventMorning";
+                console.log("event", event);
+                event.backgroundColor = "rgba(255,255,255,0.8)"
+                event.borderColor = "rgba(255,255,255,0.8)"
                 $scope.events.push(event);
             });
-        });
-    };
+    }
 
 
-    $scope.getUserBrushingData();
+    $scope.populateBrushingData();
 
     var date = new Date();
     var d = date.getDate();
@@ -77,7 +77,8 @@ app.controller('brushingMoreCtrl', function ($scope, $location, $rootScope, $rou
     /* config object */
     $scope.uiConfig = {
       calendar:{
-        borderColor: "#000000",
+        defaultView: "basicWeek",
+        borderColor: "rgba(255,255,255,1)",
         height: 450,
         contentHeight: 400,
         editable: true,
@@ -89,7 +90,8 @@ app.controller('brushingMoreCtrl', function ($scope, $location, $rootScope, $rou
         eventClick: $scope.alertOnEventClick,
         eventDrop: $scope.alertOnDrop,
         eventResize: $scope.alertOnResize,
-        eventRender: $scope.eventRender
+        eventRender: $scope.eventRender,
+        eventbackgroundColor: "#000000"
       }
     };
 
